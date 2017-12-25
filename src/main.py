f0b2f68e79bf4376e2babaf3ce2bba66de89c6b2
pram <rizki.ihza@gmail.com>
2017-12-25 12:58:03 +0700

    finalisasai fungsionalitas terminal

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ import httplib2
 import pandas as pd
 import saham
 import numpy as np
+import random
 
-x = saham.predict_tomorrow_price("BBRI")
-print(x)
+saham_label = input("masukan simbol saham: ")
+data_df = saham.get_data_saham(saham_label)
+print(data_df["Close"][-30:].to_string(index=False))
+hasil = saham.predict_tomorrow_price(saham_label)
+print(hasil)
--- src/saham.py
+++ src/saham.py
@@ -25,7 +25,7 @@ def preprocessing(df):
     
     return df
 
-def include_shift(stockdf, days):
+def include_shift(stockdf, days = 8):
     #buat harga close beberapa hari menjadi fitur
     for i in range(1,days+1):
         length_forecast=i
@@ -40,23 +40,50 @@ def predict_tomorrow_price(saham_label, days = 8):
     maindf = include_shift(maindf, days)
     # maindf = include_shift(preprocessing(maindf), days)
     regressor = linear_model.Ridge(alpha=1.0)
-
+    
     x_df = maindf.drop('shifted_close_' + str(days),1)
     y_df = maindf['shifted_close_' + str(days)]
 
-    x = np.array(maindf.drop(['shifted_close_' + str(days)], 1))
-    y = np.array(maindf['shifted_close_' + str(days)])
+    x = np.array(x_df)
+    y = np.array(y_df)
 
     x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size = 0.1)
     regressor.fit(x_train, y_train)
     
-    hasil = regressor.score(x_test, y_test)
-    return hasil*100
+    #prepare test case for result
+    x_result = []
+    for i in range(1, days+1):
+        column = 'shifted_close_' + str(i)
+        x_result.append(maindf[column][len(maindf[column]) - 1])
+    
+    return regressor.predict(np.array(x_result).reshape(1, -1))
+
+
     
 def predict_tomorrow_price_preprocessed(saham_label, days = 8):
     maindf = get_data_saham(saham_label)
+    maindf = preprocessing(maindf)
     maindf = include_shift(maindf, days)
 
-    regressor = linear_model.LinearRegression()
+    regressor = svm.SVR()
+
+    x_df = maindf.drop('shifted_close_' + str(days), 1)
+    y_df = maindf['shifted_close_' + str(days)]
+    
+    x = np.array(x_df)
+    y = np.array(y_df)
+
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size = 0.1)
+    regressor.fit(x_train, y_train)
+
+    hasil = regressor.score(x_test, y_test)
+
+    #prepare data for prediction
+    x_result = []
+    for i in range(1, days+1):
+        column = 'shifted_close_' + str(i)
+        x_result.append(maindf[column][len(maindf[column])-1])
+
+    return regressor.predict(np.array(x_result).reshape(1, -1))
 
 
